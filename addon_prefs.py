421d689cf62ae23c057f613afd8636a3d02bcaaf
Dg0 <40403757+PAL123@users.noreply.github.com>
2026-04-24 11:31:05 +0200

    Implement custom font folder management in preferences
    Added functionality to manage custom font folders in the add-on preferences, including operators to add and remove folders.

--- addon_prefs.py
+++ addon_prefs.py
@@ -1,9 +1,46 @@
 import bpy
 import os
 
+# --- 1. DATENSTRUKTUR FÜR EINEN ORDNER ---
+class FONTSELECTOR_PG_custom_folder(bpy.types.PropertyGroup):
+    path: bpy.props.StringProperty(
+        name="Path",
+        description="Zusätzlicher Ordner, der beim Font-Scan berücksichtigt wird",
+        subtype="DIR_PATH",
+        default="",
+    )
 
-class FONTSELECTOR_PF_addon_prefs(bpy.types.AddonPreferences) :
+# --- 2. OPERATOR: ORDNER HINZUFÜGEN ---
+class FONTSELECTOR_OT_add_folder(bpy.types.Operator):
+    bl_idname = "fontselector.add_folder"
+    bl_label = "Add Font Folder"
+    bl_description = "Fügt einen neuen Ordner-Pfad hinzu"
+    
+    def execute(self, context):
+        get_addon_preferences().custom_font_folders.add()
+        return {'FINISHED'}
+
+# --- 3. OPERATOR: ORDNER ENTFERNEN ---
+class FONTSELECTOR_OT_remove_folder(bpy.types.Operator):
+    bl_idname = "fontselector.remove_folder"
+    bl_label = "Remove Font Folder"
+    bl_description = "Entfernt diesen Ordner-Pfad"
+    
+    index: bpy.props.IntProperty() # Speichert, welcher Ordner gelöscht werden soll
+    
+    def execute(self, context):
+        get_addon_preferences().custom_font_folders.remove(self.index)
+        return {'FINISHED'}
+
+
+# --- 4. DEINE ANGEPASSTEN PREFERENCES ---
+class FONTSELECTOR_PF_addon_prefs(bpy.types.AddonPreferences):
     bl_idname = __package__
+    
+    # HIER: Die neue CollectionProperty nutzt unsere PropertyGroup von oben
+    custom_font_folders: bpy.props.CollectionProperty(
+        type=FONTSELECTOR_PG_custom_folder
+    )
     
     preferences_folder: bpy.props.StringProperty(
         name = "Preferences folder",
@@ -16,7 +53,6 @@ class FONTSELECTOR_PF_addon_prefs(bpy.types.AddonPreferences) :
     )
     viewport_popover : bpy.props.BoolProperty(
         name = "3D Viewport Popover",
-        # default = True,
     )
     properties_panel : bpy.props.BoolProperty(
         name = "Font Properties Panel",
@@ -24,7 +60,6 @@ class FONTSELECTOR_PF_addon_prefs(bpy.types.AddonPreferences) :
     )
     sequencer_popover : bpy.props.BoolProperty(
         name = "Sequencer Popover",
-        # default = True,
     )
     sequencer_panel : bpy.props.BoolProperty(
         name = "Sequencer Properties Panel",
@@ -34,9 +69,26 @@ class FONTSELECTOR_PF_addon_prefs(bpy.types.AddonPreferences) :
         name = "Pop Up Operator",
     )
 
-
-    def draw(self, context) :
+    def draw(self, context):
         layout = self.layout
+        
+        # --- UI für die Custom Folders ---
+        box = layout.box()
+        box.label(text="Custom Font Folders:", icon='FILE_FOLDER')
+        
+        # Alle gespeicherten Ordner auflisten
+        for i, folder in enumerate(self.custom_font_folders):
+            row = box.row()
+            row.prop(folder, "path", text="")
+            # X-Button zum Löschen der jeweiligen Zeile
+            op = row.operator("fontselector.remove_folder", text="", icon='X')
+            op.index = i
+            
+        # Plus-Button zum Hinzufügen einer neuen Zeile
+        box.operator("fontselector.add_folder", text="Add Folder", icon='ADD')
+        # ---------------------------------
+        
+        layout.separator()
         
         row = layout.row()
         row.prop(self, "preferences_folder", text="Preferences")
@@ -44,9 +96,9 @@ class FONTSELECTOR_PF_addon_prefs(bpy.types.AddonPreferences) :
         sub.alignment = "RIGHT"
         sub.prop(self, "debug")
         
-        box = layout.box()
-        box.label(text="UI")
-        col = box.column(align=True)
+        box_ui = layout.box()
+        box_ui.label(text="UI")
+        col = box_ui.column(align=True)
         col.prop(self, "viewport_popover")
         col.prop(self, "properties_panel")
         col.separator()
@@ -56,15 +108,22 @@ class FONTSELECTOR_PF_addon_prefs(bpy.types.AddonPreferences) :
         col.prop(self, "popup_operator")
         
 
-# get addon preferences
 def get_addon_preferences():
     addon = bpy.context.preferences.addons.get(__package__)
     return getattr(addon, "preferences", None)
 
+# Alle Klassen müssen registriert werden (Reihenfolge ist wichtig!)
+classes = (
+    FONTSELECTOR_PG_custom_folder,
+    FONTSELECTOR_OT_add_folder,
+    FONTSELECTOR_OT_remove_folder,
+    FONTSELECTOR_PF_addon_prefs,
+)
 
-### REGISTER ---
 def register():
-    bpy.utils.register_class(FONTSELECTOR_PF_addon_prefs)
+    for cls in classes:
+        bpy.utils.register_class(cls)
 
 def unregister():
-    bpy.utils.unregister_class(FONTSELECTOR_PF_addon_prefs)
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
